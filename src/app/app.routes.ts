3f4bc42a27dfa0e9ebb49300ddff837772b67615
jfpa11100 <felipepala1000@gmail.com>
2025-08-25 14:20:02 -0500

    Global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { SignupComponent } from './auth/pages/signup/signup.component';
+import { HomeComponent } from './main/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
+];
